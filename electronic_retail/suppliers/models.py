5c2fa784ab946e4ba5d83ad49a7be664a393a7dd
yakhovets.o <yakhovetso@gmail.com>
2025-07-17 21:21:43 +0300

    add `Address` model with basic fields

--- electronic_retail/suppliers/models.py
+++ electronic_retail/suppliers/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Address(models.Model):
+    """Модель для хранения адресов.
+
+    Атрибуты:
+        country: страна
+        city: город
+        street: улица
+        house_number: номер дома
+    """
+
+    country = models.CharField(max_length=100, verbose_name="Страна")
+    city = models.CharField(max_length=100, verbose_name="Город")
+    street = models.CharField(max_length=255, verbose_name="Улица")
+    house_number = models.CharField(max_length=50, verbose_name="Номер дома")
+
+    def __str__(self):
+        return (
+            f"{self.country}, {self.city} "
+            f"{self.street}, {self.house_number}"
+        )
